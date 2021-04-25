498724f749f02ebc50d3c2bb369a52b0dedb8b15
tyger796 <tyger796@outlook.com>
2021-04-25 13:58:31 +0100

    added autopull system

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,23 @@
+const exec = require('child_process').exec;
+
 module.exports = async (client) => {
     console.log(`Logged into Discord Successfully!`)
     console.log(`Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`);
-    
     client.user.setActivity(client.config.discord.activity);
+
+    setInterval(() => {
+        exec(`git pull`, (error, stdout) => {
+            let response = (error || stdout);
+            if (!error) {
+                if (response.includes("Already up to date.")) {
+                    console.log('Bot is up to date. No changes since last pull.')
+                } else {
+                    client.channels.cache.get('825827995507490886').send('**[Automatic]** \nNew update on GitHub. Pulling... \n\n Logs: \n```' + response + "```" + "\n\n**Restarting bot**");
+                    setTimeout(() => {
+                        process.exit();
+                    }, 1000)
+                }
+            }
+        })
+    }, 10000)
 };
