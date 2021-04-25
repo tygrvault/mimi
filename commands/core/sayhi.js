ed10a2373ff45d60a57d8b1821111c80162e9b8d
tyger796 <tyger796@outlook.com>
2021-04-25 19:22:23 +0100

    Update sayhi.js

--- commands/core/sayhi.js
+++ commands/core/sayhi.js
@@ -8,7 +8,7 @@ module.exports = {
             embed: {
                 color: 'D8DEE9',
                 author: { name: 'Hi!' },
-                description: 'Hey Instagram, My name is Mimi and I am a free and open source Discord music bot. If you wish to invite me to your server, please go to "mimi.nordstudios.org" or just visit the projects page on the Nord Studios website (link in bio).\n\nHappy to join the Nord family :heart:\nSee you soon 😉',
+                description: 'Hey Twitter, My name is Mimi and I am a free and open source Discord music bot. If you wish to invite me to your server, please go to "mimi.nordstudios.org" or just visit the projects page on the Nord Studios website (link in bio).\n\nHappy to join the Nord family :heart:\nSee you soon 😉',
                 footer: { text: 'SHH -> mimi.nordstudios.org/shh' }
             }
         })
