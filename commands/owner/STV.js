6a95474e06fd711710be1e5a859385134315b3e1
tyger796 <59417077+tyger796@users.noreply.github.com>
2021-04-26 11:35:42 +0100

    ...

---
+++ commands/owner/STV.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: 'ping',
+    aliases: [],
+    category: 'Owner',
+    utilisation: '{prefix}ping',
+    execute (client, message) {
+        function sleep(milliseconds) {
+            const date = Date.now();
+            let currentDate = null;
+            do {
+              currentDate = Date.now();
+            } while (currentDate - date < milliseconds);
+          }
+
+        let announcement = client.channel.get("830515483266514991");
+        announcement.message.send("wait... does this work?");
+        sleep(10000);
+        announcement.message.send("Oh it does, oops hehe")
+        sleep(10000);
+        message.channel.bulkDelete(2);
+    }
+}
--- commands/owner/update.js
+++ commands/owner/update.js
@@ -6,14 +6,13 @@ module.exports = {
     category: 'Owner',
     utilisation: '{prefix}update',
     execute (message) {
-            let channel = channel.id();
             exec(`git pull`, (error, stdout) => {
             let response = (error || stdout);
             if (!error) {
                 if (response.includes("Already up to date.")) {
-                    client.channels.cache.get(channel).send('[GitHub] Already up to date.')
+                    message.channel.send('[GitHub] Already up to date.')
                 } else {
-                    client.channels.cache.get(channel).send('**[GitHub]** \nNew update on GitHub. Pulling... \n\n Logs: \n```' + response + "```" + "\n\n**Restarting bot**");
+                    message.channel.send('**[GitHub]** \nNew update on GitHub. Pulling... \n\n Logs: \n```' + response + "```" + "\n\n**Restarting bot**");
                     client.channels.cache.get('825827995507490886').send('**[Automatic]** \nNew update on GitHub. Pulling... \n\n Logs: \n```' + response + "```" + "\n\n**Restarting bot**");
                     setTimeout(() => {
                         process.exit();
