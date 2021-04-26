939c33f16eaea06861132bd91e598ac42f576600
tyger796 <59417077+tyger796@users.noreply.github.com>
2021-04-26 11:04:38 +0100

    Update update.js

--- commands/owner/update.js
+++ commands/owner/update.js
@@ -6,13 +6,14 @@ module.exports = {
     category: 'Owner',
     utilisation: '{prefix}update',
     execute (message) {
+            let channel = message.guild.id();
             exec(`git pull`, (error, stdout) => {
             let response = (error || stdout);
             if (!error) {
                 if (response.includes("Already up to date.")) {
-                    message.channel.send("[GitHub] Already up to date.")
+                    client.channels.cache.get(channel).send('[GitHub] Already up to date.')
                 } else {
-                    message.channel.send('**[GitHub]** \nNew update on GitHub. Pulling... \n\n Logs: \n```' + response + "```" + "\n\n**Restarting bot**");
+                    client.channels.cache.get(channel).send('**[GitHub]** \nNew update on GitHub. Pulling... \n\n Logs: \n```' + response + "```" + "\n\n**Restarting bot**");
                     client.channels.cache.get('825827995507490886').send('**[Automatic]** \nNew update on GitHub. Pulling... \n\n Logs: \n```' + response + "```" + "\n\n**Restarting bot**");
                     setTimeout(() => {
                         process.exit();
