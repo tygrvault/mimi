7fb74fa8aa3729e6d39450da92bf8c74c869a970
tyger796 <tyger796@outlook.com>
2021-05-11 23:29:03 +0100

    yes

--- commands/owner/yes.js
+++ commands/owner/yes.js
@@ -4,11 +4,6 @@ module.exports = {
     category: 'Owner',
     utilisation: '{prefix}yes',
     execute(message) {
-        message.channel.send({embed: {
-            title: ':)',
-            image: {
-                url: 'https://canary.discord.com/assets/da3651e59d6006dfa5fa07ec3102d1f3.svg',
-            },
-        }});
+        message.channel.send("Yes!")
     }
 }
