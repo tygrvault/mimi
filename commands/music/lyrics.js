5b99dad3d9dadcb4fb8592e32cc1aadcb5a7034e
tyger796 <tyger796@outlook.com>
2021-05-21 23:27:36 +0100

    debug mode

--- commands/music/lyrics.js
+++ commands/music/lyrics.js
@@ -5,9 +5,10 @@ module.exports = {
     utilisation: '{prefix}lyrics',
 
     execute(client, message) {
-        if (!message.member.voice.channel) return message.channel.send(`You're not in a voice channel!`);
+        /* if (!message.member.voice.channel) return message.channel.send(`You're not in a voice channel!`);
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`You are not in the same voice channel!`);
-        
+        */
+       
         const lyrics = await client.player.lyrics(args[0])
         message.channel.send(lyrics.lyrics);
     }
