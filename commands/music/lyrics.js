4db80b89ccbd4602d92d942796b36177c3427447
tyger796 <tyger796@outlook.com>
2021-05-21 23:30:17 +0100

    made an async function

--- commands/music/lyrics.js
+++ commands/music/lyrics.js
@@ -8,8 +8,10 @@ module.exports = {
         /* if (!message.member.voice.channel) return message.channel.send(`You're not in a voice channel!`);
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`You are not in the same voice channel!`);
         */
-       
-        const lyrics = await client.player.lyrics(args[0])
-        message.channel.send(lyrics.lyrics);
+        async function lyrics() {
+            const lyrics = await client.player.lyrics(args[0])
+            message.channel.send(lyrics.lyrics);
+        };
+        lyrics();
     }
 }
