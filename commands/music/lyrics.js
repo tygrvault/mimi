1cae03e669d5061996dc6962f25b4e0a6a918cda
tyger796 <tyger796@outlook.com>
2021-05-21 23:24:49 +0100

    Update lyrics.js

--- commands/music/lyrics.js
+++ commands/music/lyrics.js
@@ -7,7 +7,8 @@ module.exports = {
     execute(client, message) {
         if (!message.member.voice.channel) return message.channel.send(`You're not in a voice channel!`);
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`You are not in the same voice channel!`);
-
-        message.react('🔜');
+        
+        const lyrics = await client.player.lyrics(args[0])
+        message.channel.send(lyrics.lyrics);
     }
 }
