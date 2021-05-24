93487025158ecb1e2be0a0c68e5520893dddf85f
tyger796 <tyger796@outlook.com>
2021-05-24 21:15:58 +0100

    Fix lyrics command

--- commands/music/lyrics.js
+++ commands/music/lyrics.js
@@ -4,14 +4,11 @@ module.exports = {
     category: 'Music',
     utilisation: '{prefix}lyrics',
 
-    execute(client, message) {
-        /* if (!message.member.voice.channel) return message.channel.send(`You're not in a voice channel!`);
+    async execute(client, message) {
+        if (!message.member.voice.channel) return message.channel.send(`You're not in a voice channel!`);
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`You are not in the same voice channel!`);
-        */
-        async function lyrics() {
-            const lyrics = await client.player.lyrics(args[0])
-            message.channel.send(lyrics.lyrics);
-        };
-        lyrics();
+        
+        const lyrics = await client.player.lyrics(args[0])
+        message.channel.send(lyrics.lyrics);
     }
 }
