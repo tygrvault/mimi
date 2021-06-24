605b9cb770bc7bda855a40042e9b8e0dabe2fa82
tyger796 <tyger796@outlook.com>
2021-06-24 17:12:17 +0100

    Update

--- run.bat
+++ run.bat
@@ -1,5 +1,12 @@
 @echo off
-echo Starting..
+:start
+if exist node_modules\ (
+    echo Starting bot...
+) else (
+    echo Installing packages...
+    npm install
+    goto start
+)
 :main
 node index.js
 echo OwO what's this, the bot crashed, restarting it ;3
